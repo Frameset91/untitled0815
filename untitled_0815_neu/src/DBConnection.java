e43c01ffa9ff92b0c02712c015a93b73761dd149
bjoernl89 <bjoern.list@gmail.com>
2012-09-18 00:04:50 +0200

    comments

--- untitled_0815_neu/src/DBConnection.java
+++ untitled_0815_neu/src/DBConnection.java
@@ -9,7 +9,7 @@ public class DBConnection {
 	 */
 	private DBConnection() {
 		connect();
-	
+
 	}
 
 	/**
@@ -24,42 +24,64 @@ public class DBConnection {
 		return singleton;
 	}
 
+	/**
+	 * Verbindet sich mit Der Datenbank
+	 */
 	private void connect() {
 		try {
 			Class.forName("org.hsqldb.jdbcDriver");
 			// Aufbauen der Verbindung zu der Datenbank
-			this.con = DriverManager
-					.getConnection("jdbc:hsqldb:hsql://localhost/",
-							"SA", "");
-			
+			this.con = DriverManager.getConnection(
+					"jdbc:hsqldb:hsql://localhost/", "SA", "");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void sendSelectStatement(String sql) {
-		try{
-		Statement stmnt = this.con.createStatement();
-		ResultSet result = stmnt.executeQuery(sql);
-		}
-		catch(Exception e){
+	/**
+	 * Sendet ein SelectStatement an die Datenbank
+	 * 
+	 * @param sql
+	 *            Query als String
+	 * @return ResultSet sendet ein Resultset zurueck - Wenn kein Ergebnis ist
+	 *         null
+	 */
+	public ResultSet sendSelectStatement(String sql) {
+		ResultSet result = null;
+		try {
+			Statement stmnt = this.con.createStatement();
+			result = stmnt.executeQuery(sql);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		return result;
+
 	}
-	
-	public void sendOtherStatement( String sql){
-		try{
+
+	/**
+	 * Sendet ein anderes Statement, das keine Rueckgabe erzeugt
+	 * 
+	 * @param sql
+	 *            Query als String
+	 */
+
+	public void sendOtherStatement(String sql) {
+		try {
 			Statement stmnt = this.con.createStatement();
 			boolean result = stmnt.execute(sql);
-			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
-	public static void main(String[] args){
+
+	/**
+	 * nur zu Testzwecken
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
 		DBConnection bla = getInstance();
 		bla.sendOtherStatement("CREATE TABLE blubblub (spalte1 INTEGER)");
 	}
