d717b84829633ef00d97926713e7401caeed8a00
bjoernl89 <bjoern.list@gmail.com>
2012-09-17 23:54:39 +0200

    Ich habe für den DB COnnector schonmal vor gearbeite :P

--- untitled_0815_neu/src/DBConnection.java
+++ untitled_0815_neu/src/DBConnection.java
@@ -1,22 +1,67 @@
+import java.sql.*;
 
 public class DBConnection {
-private static DBConnection singleton = null;
+	private static DBConnection singleton = null;
+	private Connection con;
 
-/**
- * privater Konstruktor
- */
-private DBConnection(){
+	/**
+	 * privater Konstruktor
+	 */
+	private DBConnection() {
+		connect();
 	
-}
-
-/**
- * Liefert die refernz auf Singleton Instanz zurueck
- * @return Referenz auf die SIngletonInstanz
- */
-public static DBConnection getInstance(){
-	if (singleton == null){
-		singleton = new DBConnection();
 	}
-	return singleton;
-}
+
+	/**
+	 * Liefert die refernz auf Singleton Instanz zurueck
+	 * 
+	 * @return Referenz auf die SIngletonInstanz
+	 */
+	public static DBConnection getInstance() {
+		if (singleton == null) {
+			singleton = new DBConnection();
+		}
+		return singleton;
+	}
+
+	private void connect() {
+		try {
+			Class.forName("org.hsqldb.jdbcDriver");
+			// Aufbauen der Verbindung zu der Datenbank
+			this.con = DriverManager
+					.getConnection("jdbc:hsqldb:hsql://localhost/",
+							"SA", "");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void sendSelectStatement(String sql) {
+		try{
+		Statement stmnt = this.con.createStatement();
+		ResultSet result = stmnt.executeQuery(sql);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void sendOtherStatement( String sql){
+		try{
+			Statement stmnt = this.con.createStatement();
+			boolean result = stmnt.execute(sql);
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+	}
+	
+	
+	public static void main(String[] args){
+		DBConnection bla = getInstance();
+		bla.sendOtherStatement("CREATE TABLE blubblub (spalte1 INTEGER)");
+	}
+
 }
