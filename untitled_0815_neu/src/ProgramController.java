91fde0f5a7145a3c948032f470fbc3e65611d1e4
Sascha <Sascha Ulbrich@SU>
2012-09-19 21:02:14 +0200

    jo

--- untitled_0815_neu/src/ProgramController.java
+++ untitled_0815_neu/src/ProgramController.java
@@ -1,12 +1,24 @@
 
 public class ProgramController {
 
+	private GameView gameView;
+	private GameModel gameModel;
+	
+	public enum gameRole{
+		x,o
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub--- passt!
+		// TODO Auto-generated method stub
 
+	}
+	
+	private void newGame(int cols, int rows, gameRole role, int sets){
+		gameModel = new GameModel(); 
+		
 	}
 
 }
