c9c631afa1333a4d75144d4be0bc1d8e1d2d165b
JohannesR <nomailhere@example.org>
2012-09-24 10:04:39 +0200

    neue Constants-Klasse

---
+++ untitled_0815_neu/src/Constants.java
@@ -0,0 +1,11 @@
+/**
+ * Hier werden Settings und Konstanten gesammelt
+ * @author Johannes
+ * 
+ */
+
+public class Constants {
+	static final Boolean self = true;
+	static final Boolean opp = false;
+	static final Boolean empty = null;
+}
