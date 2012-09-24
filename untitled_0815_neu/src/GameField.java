ad2483a690db34750e28574301d7f4347a3516da
Sascha <Sascha Ulbrich@SU>
2012-09-24 10:08:38 +0200

    GameField implementier

---
+++ untitled_0815_neu/src/GameField.java
@@ -0,0 +1,22 @@
+
+public class GameField {
+	private Boolean[][] field;
+	
+	public GameField(int cols, int rows){
+		field = new Boolean[cols][rows];
+		
+	}
+	
+	public void addMove(Move move, Game.GameRole ownRole){
+		int col = move.getColumn();
+		for(int i=0; i<field[col].length; i++){
+			if (field[col][i] == null){
+				if(move.getRole() == ownRole)
+					field[col][i]= true;
+				else
+					field[col][i]= false;
+				break;
+			}
+		}		
+	}
+}
