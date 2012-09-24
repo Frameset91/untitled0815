d5e558fb9ba2cd3e425c5e2f73813e044f948b8b
JohannesR <nomailhere@example.org>
2012-09-24 11:55:03 +0200

    gamefieldrow/column-Konstante + Wörterbuch-Eintrag

--- untitled_0815_neu/src/Constants.java
+++ untitled_0815_neu/src/Constants.java
@@ -1,12 +1,13 @@
 /**
- * afuhafuafasdjhk
- * SaschaHier werden Settings und Konstanten gesammelt
- * @author Johannes
- * test
+ * Hier werden Settings und Konstanten gesammelt
+ * @author Johannes Riedel
  */
 
 public class Constants {
 	static final Boolean self = true;
 	static final Boolean opp = false;
 	static final Boolean empty = null;
+	
+	static final int gamefieldcolumncount = 7;
+	static final int gamefieldrowcount = 6;
 }
