13c085c5855cdc06bcaff73124e3fde7978b131a
bjoernl89 <bjoern.list@gmail.com>
2012-10-01 11:31:46 +0200

    events dreck

---
+++ untitled_0815_neu/src/utilities/EventListener.java
@@ -0,0 +1,9 @@
+package utilities;
+
+
+/**
+ * @author Stephan Schmidt <stephan.schmidt@schlund.de>
+ */
+public interface EventListener {
+    public void handleEvent(Event e) throws Exception;
+}
