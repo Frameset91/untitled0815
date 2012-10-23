81e2ad03064c03d0cd36a64f36c8cdf5ec15047c
bjoernl89 <bjoern.list@gmail.com>
2012-10-23 23:44:28 +0200

    blub

--- untitled_0815_neu/src/core/GameController.java
+++ untitled_0815_neu/src/core/GameController.java
@@ -426,13 +426,7 @@ public class GameController implements GameEventListener, Observer{
 		EventDispatcher Dispatcher = EventDispatcher.getInstance();
 		try {			
 			Dispatcher.addListener(this);
-//			Dispatcher.addListener(GameEvent.Type.EndGame.toString(), this);
-//			Dispatcher.addListener(GameEvent.Type.EndSet.toString(), this);
-//			Dispatcher.addListener(GameEvent.Type.LoadGame.toString(), this);
-//			Dispatcher.addListener(GameEvent.Type.StartSet.toString(), this);
-//			Dispatcher.addListener(GameEvent.Type.OppMove.toString(), this);
-//			Dispatcher.addListener(GameEvent.Type.WinnerSet.toString(), this);
-			
+		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}		
