b583d6c089b8f6c9f84f62558ec31707503efefb
Sascha <Sascha Ulbrich@SU>
2012-10-25 21:44:00 +0200

    noRole auf +/- geändert

--- untitled_0815_neu/src/core/Constants.java
+++ untitled_0815_neu/src/core/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 	
 	public static final char xRole = 'X';
 	public static final char oRole = 'O';
-	public static final char noRole = ' ';
+	public static final char noRole = (char)177;
 	public static final char defaultRole = xRole;
 	
 	public static final int defaultTimeoutServer = 300;
