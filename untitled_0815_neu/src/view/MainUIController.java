778a5f2b44f751c20f3280d730091110e5e021d3
Sascha <Sascha@Sascha-PC>
2012-10-26 21:16:26 +0200

    Styles für Token

--- untitled_0815_neu/src/view/MainUIController.java
+++ untitled_0815_neu/src/view/MainUIController.java
@@ -155,6 +155,7 @@ public class MainUIController implements Initializable{
 	        spielfeld[i][j] = new Label(" ");
             spielfeld[i][j].prefHeightProperty().set(40);
 	        spielfeld[i][j].prefWidthProperty().set(40); 
+	        spielfeld[i][j].getStyleClass().add("token");
 	        spielfeld[i][j].styleProperty().bindBidirectional(viewModel.field()[i][Constants.gamefieldrowcount -1 -j], new StyleConverter());
 	        spielfeld[i][j].textProperty().bindBidirectional(viewModel.field()[i][Constants.gamefieldrowcount -1 -j], new TokenRoleConverter());
 	        feld.add(spielfeld[i][j], i, j);
@@ -622,39 +623,18 @@ public class MainUIController implements Initializable{
 			//Property hat sich verändert -> UI anpassen
 			String style ="";
 			if(arg0 != null){
-					switch (arg0.charAt(0)) {
-						case Constants.xRole:
-							style="-fx-background-color: red; " +
-									"-fx-background-radius: 20; " +
-									"-fx-font-size: 15; " +
-									"-fx-effect: innershadow(two-pass-box, grey, 10, 0.5, 0, 0); " +
-									"-fx-alignment: center;" + 
-									"-fx-font-weight: bold;" +
-									"-fx-text-fill: RGB(100,100,100,0.5);";
-							break;
-						case Constants.oRole:
-							style="-fx-background-color: yellow; " +
-									"-fx-background-radius: 20; " +
-									"-fx-font-size: 15; " +
-									"-fx-effect: innershadow(two-pass-box, grey, 10, 0.5, 0, 0); " +
-									"-fx-alignment: center;" +
-									"-fx-font-weight: bold;" +
-									"-fx-text-fill: RGB(100,100,100,0.5);";
-							break;
-						case Constants.noRole:
-							style="-fx-background-color: white; " +
-									"-fx-background-radius: 20; " +
-									"-fx-font-size: 15; " +
-									"-fx-effect: innershadow(two-pass-box, grey, 10, 0.5, 0, 0); " +
-									"-fx-alignment: center;" + 
-									"-fx-font-weight: bold;" +
-									"-fx-text-fill: RGB(100,100,100,0.5);";
-							break;
-						default:
-							break;
-					}
+				switch (arg0.charAt(0)) {
+					case Constants.xRole:
+						style="-fx-background-color: red;";
+						break;
+					case Constants.oRole:
+						style="-fx-background-color: yellow;";
+						break;
+					default:
+						break;
+				}
 				if( arg0.length() > 1 && arg0.charAt(1) == Constants.winMarker){
-					style += "-fx-text-fill: RGB(0,0,0);";
+					style += "-fx-text-fill: RGB(0,0,0); -fx-effect: innershadow(gaussian, green, 10, 0.5, 0, 0);";
 				}
 			}
 			return style;
